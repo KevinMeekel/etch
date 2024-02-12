5703b31d986bba5ff29d89211b6f97fab0943a80
KevinMeekel <144624233+KevinMeekel@users.noreply.github.com>
2024-02-12 01:48:47 +0100

    Change input to user prompt

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let axisLength = 16;
 
 const gridContainer = document.querySelector(".grid-container");
-gridContainer.style = "display: flex; flex-direction: column; background-color: green; gap: 3px";
+gridContainer.style = "display: flex; flex-direction: column; background-color: green; gap: 1px";
 
 const block = document.createElement("div");
 
 function createGrid(length) {
+    gridContainer.innerHTML = "";
+    
     for (let i = 0; i < length; i++) {
         const gridRow = document.createElement("div");
-        gridRow.style = "display: flex; justify-content: space-evenly; width: 100%; flex-grow: 1; gap: 3px";
+        gridRow.style = "display: flex; justify-content: space-evenly; width: 100%; flex-grow: 1; gap: 1px";
         gridRow.classList.add("row");
         gridContainer.appendChild(gridRow);
     }
@@ -30,5 +32,11 @@ function createGrid(length) {
     }
 }
 
-
 createGrid(axisLength);
+
+const setButton = document.querySelector(".set");
+setButton.addEventListener("click", (e) => {
+    const input = prompt("type in a value between 1 and 100");
+    axisLength = input;
+    createGrid(axisLength);
+});
